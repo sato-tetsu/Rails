d5916e504a235dfe48af2b162451b9c2e40ef819
佐藤哲 <satouakira@satouakiranoMacBook.local>
2020-05-21 14:06:08 +0900

    [Add]Rails

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '358e9be8b95330bae1712cab5330f65dbadd5babd51dcf73a2b964f90d761ffe7247e8e3fb7aeb8d0524e8cd784fa13c84a2853f5abab5a8060da9a128aa29e2'
